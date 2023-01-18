8948498a32c11cf22006a9713e1a5b27315653c3
Pierce <pierceevans@me.com>
2023-01-17 18:51:04 -0600

    commit tree 5c9698c3b8ddb1c805d916f203c5cc2f7800cb9f

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-function isPalindrome(word) {
+function isPalindrome(str) {
   // Write your algorithm here
-}
 
-/* 
-  Add your pseudocode here
+    var re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+      if (str[i] !== str[len - 1 - i]) {
+          return false;
+      }
+    }
+    return true;
+   }
+   isPalindrome("A man, a plan, a canal. Panama");
+
+
+/* if(palendrome) {
+  return true
+}
+  else{
+    return false
+  }
 */
 
 /*
-  Add written explanation of your solution here
+  Set rules to a variable to allow for specific inputs.
+  Break down algorithm for palendromes.
+  Use if statement to dictate output.
 */
 
 // You can run `node index.js` to view these console logs
